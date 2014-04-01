36ee004057f4131008a02fad62345ca1162690c0
Jonathan Mukai-Heidt & Kevin Wolkober <nochat-development@googlegroups.com>
2014-04-01 12:03:33 -0400

    Serve static assets in production
    Heroku expects our app to handle static assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Nochat::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Serve static assets because Heroku needs the app to serve these
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
