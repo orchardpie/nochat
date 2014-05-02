5bd02c08e2b5052f5e10f154de1061209f70c228
Jonathan Mukai-Heidt & Kevin Wolkober <pair+am+kw@orchardpie.com>
2014-05-02 14:08:41 -0400

    Use SendGrid for e-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ NoChat::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Necessary for Devise
+  config.action_mailer.default_url_options = { host: 'nochat-dev.herokuapp.com' }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
